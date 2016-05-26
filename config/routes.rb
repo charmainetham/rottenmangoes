a29dfd583a1bcac81c8d136b87e3e4c003b6364c
charmainetham <charmainetham@gmail.com>
2016-05-26 02:50:50 +0000

    adding the concept of admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 RottenTomatoes::Application.routes.draw do
+  get 'admin/index'
+
+  get 'admin/users'
+
+  get 'admin/new'
+
+  get 'admin/show'
+
+  get 'admin/edit'
+
+  get 'admin/destroy'
+
+  get 'admin/users'
+
+  get 'admin/show'
+
+  get 'admin/edit'
+
+  get 'admin/destroy'
+
   get "reviews/new"
   get "reviews/create"
   get "sessions/new"
@@ -11,4 +31,8 @@ RottenTomatoes::Application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
+
+  namespace :admin do
+    resources :users
+  end
 end
